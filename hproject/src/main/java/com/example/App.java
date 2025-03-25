6ef912ae6472aef5b5a319f772b1611f7114b5eb
csproles <c.sproles2005@gmail.com>
2025-03-25 09:54:15 -0500

    added a button that does nothing

--- hproject/src/main/java/com/example/App.java
+++ hproject/src/main/java/com/example/App.java
@@ -1,6 +1,9 @@
 package com.example;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +15,13 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage){
-        
+        StackPane pane = new StackPane();
+        pane.getChildren().add(new Button("Button"));
+        Scene scene1 = new Scene(pane, 500, 500);
+        primaryStage.setTitle("Show Line");
+        primaryStage.setScene(scene1);
+
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
