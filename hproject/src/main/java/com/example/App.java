993495eaf948359416eb4675b6703a5b94cb88f9
csproles <c.sproles2005@gmail.com>
2025-03-25 09:32:58 -0500

    added javafx to the project
    -i did it in a slightly weird way so it might have a few bugs, but i don't think it will 😎

---
+++ hproject/src/main/java/com/example/App.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage){
+        
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
