1df0b1a40d68f7b8552cd8b32d00e10e97281b75
csproles <c.sproles2005@gmail.com>
2025-04-03 11:02:12 -0500

    Made buttons and moving character
    Working but doesn't look pretty
    IMPORTANT: We changed the plan on this commit

--- hproject/src/main/java/com/example/App.java
+++ hproject/src/main/java/com/example/App.java
@@ -1,24 +1,66 @@
 package com.example;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import java.io.IOException;
+import javafx.stage.Window;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
 
+    public double paneWidth = 1300; //1250
+    public double paneHeight = 650; //625
+
+    public double buttonSize = paneWidth/4;
+
     @Override
     public void start(Stage primaryStage){
         StackPane pane = new StackPane();
-        pane.getChildren().add(new Button("Button"));
-        Scene scene1 = new Scene(pane, 500, 500);
-        primaryStage.setTitle("Show Line");
+
+        String button1Text = "Level\n    1";
+        Button button1 = new Button(button1Text);
+        button1.setPrefSize(buttonSize, buttonSize);
+        button1.setTranslateX(-paneWidth/3);
+        button1.setStyle("-fx-border-color: e36b90; -fx-background-color: e36b52;");
+        button1.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.REGULAR , buttonSize/4));
+
+        String button2Text = "Level\n    2";
+        Button button2 = new Button(button2Text);
+        button2.setPrefSize(buttonSize, buttonSize);
+        button2.setTranslateX(0);
+        button2.setStyle("-fx-border-color: e36b90; -fx-background-color: e36b52;");
+        button2.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.REGULAR , buttonSize/4));
+
+        String button3Text = "Level\n    3";
+        Button button3 = new Button(button3Text);
+        button3.setPrefSize(buttonSize, buttonSize);
+        button3.setTranslateX(paneWidth/3);
+        button3.setStyle("-fx-border-color: e36b90; -fx-background-color: e36b52;");
+        button3.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.REGULAR , buttonSize/4));
+        
+        HandleButton1 handleButton1 = new HandleButton1();
+        button1.setOnAction(handleButton1);
+        HandleButton2 handleButton2 = new HandleButton2();
+        button2.setOnAction(handleButton2);
+        HandleButton3 handleButton3 = new HandleButton3();
+        button3.setOnAction(handleButton3);
+        
+        pane.getChildren().addAll(button1, button2, button3);
+
+        pane.setStyle("-fx-border-color: e36b52; -fx-background-color: accaa1;");
+        Scene scene1 = new Scene(pane, paneWidth, paneHeight);
+        primaryStage.setTitle("Test Pane");
         primaryStage.setScene(scene1);
 
         primaryStage.show();
@@ -28,3 +70,24 @@ public class App extends Application {
         launch();
     }
 }
+
+class HandleButton1 implements EventHandler<ActionEvent> {
+    @Override
+    public void handle(ActionEvent e) {
+        System.out.println("Button 1 clicked"); 
+    }
+}  
+
+class HandleButton2 implements EventHandler<ActionEvent> {
+    @Override
+    public void handle(ActionEvent e) {
+      System.out.println("Button 2 clicked"); 
+    }
+}  
+
+class HandleButton3 implements EventHandler<ActionEvent> {
+    @Override
+    public void handle(ActionEvent e) {
+      System.out.println("Button 3 clicked"); 
+    }
+}  
---
+++ hproject/src/main/java/com/example/CharacterMovement.java
@@ -0,0 +1,113 @@
+package com.example;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.*;
+import javafx.scene.image.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+/**
+ * Hold down an arrow key to have your hero move around the screen.
+ * Hold down the shift key to have the hero run.
+ */
+public class CharacterMovement extends Application {
+
+    public double paneWidth = 750;
+    public double paneHeight = 500;
+
+    private static final String HERO_IMAGE_LOC =
+            "http://icons.iconarchive.com/icons/raindropmemory/legendora/64/Hero-icon.png";
+
+    private Image heroImage;
+    private Node  hero;
+
+    boolean running, goNorth, goSouth, goEast, goWest;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        heroImage = new Image(HERO_IMAGE_LOC);
+        hero = new ImageView(heroImage);
+
+        Group dungeon = new Group(hero);
+
+        moveHeroTo(paneWidth / 2, paneHeight / 2);
+
+        Scene scene = new Scene(dungeon, paneWidth, paneHeight, Color.CORNFLOWERBLUE);
+
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case UP:    goNorth = true; break;
+                    case DOWN:  goSouth = true; break;
+                    case LEFT:  goWest  = true; break;
+                    case RIGHT: goEast  = true; break;
+                    case SHIFT: running = true; break;
+                }
+            }
+        });
+
+        scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case UP:    goNorth = false; break;
+                    case DOWN:  goSouth = false; break;
+                    case LEFT:  goWest  = false; break;
+                    case RIGHT: goEast  = false; break;
+                    case SHIFT: running = false; break;
+                }
+            }
+        });
+
+        stage.setScene(scene);
+        stage.show();
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                int dx = 0, dy = 0;
+
+                if (goNorth) dy -= 3;
+                if (goSouth) dy += 3;
+                if (goEast)  dx += 3;
+                if (goWest)  dx -= 3;
+                if (running) { dx *= 3; dy *= 3; }
+
+                moveHeroBy(dx, dy);
+            }
+        };
+        timer.start();
+    }
+
+    private void moveHeroBy(int dx, int dy) {
+        if (dx == 0 && dy == 0) return;
+
+        final double cx = hero.getBoundsInLocal().getWidth()  / 2;
+        final double cy = hero.getBoundsInLocal().getHeight() / 2;
+
+        double x = cx + hero.getLayoutX() + dx;
+        double y = cy + hero.getLayoutY() + dy;
+
+        moveHeroTo(x, y);
+    }
+
+    private void moveHeroTo(double x, double y) {
+        final double cx = hero.getBoundsInLocal().getWidth()  / 2;
+        final double cy = hero.getBoundsInLocal().getHeight() / 2;
+
+        if (x - cx >= 0 &&
+            x + cx <= paneWidth &&
+            y - cy >= 0 &&
+            y + cy <= paneHeight) {
+            hero.relocate(x - cx, y - cy);
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args); 
+    }
+}
